abe07f22ac44f212a6d839361c05c079c9181c61
COUGHDROP\btsm5 <bouchaso@sheridancollege.ca>
2025-01-20 15:28:38 -0500

    Started Scene and started work on player controlls. Added Rigidbody2D and 2D box colliders.

---
+++ Assets/Scripts/Player_Controlls.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class MechanicalDrawing : MonoBehaviour
+{
+    private Rigidbody2D body;
+    public int Speed = 1;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
