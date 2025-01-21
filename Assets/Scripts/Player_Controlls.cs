4554d6f2c2da9787f37d4811da3e6585de1b83a1
COUGHDROP\btsm5 <bouchaso@sheridancollege.ca>
2025-01-20 21:35:35 -0500

    Succesfully added key controlls for moveing side to side and up and down, however, up and down still need some work.

--- Assets/Scripts/Player_Controlls.cs
+++ Assets/Scripts/Player_Controlls.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
@@ -5,11 +6,15 @@ using UnityEngine;
 
 public class MechanicalDrawing : MonoBehaviour
 {
+    //a private variable called body the references Rigidbody so that the code knows to im using it.
     private Rigidbody2D body;
-    public int Speed = 1;
+    //a public variable float called speed that i can reference whenever i need the speed of the player character.
+    public float speed = 5;
+    
 
     private void Awake()
     {
+        //a statment telling the code to get the component Rigidbody2D whenever the variable body is called.
         body = GetComponent<Rigidbody2D>();
     }
 
@@ -18,12 +23,20 @@ public class MechanicalDrawing : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if 
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //in order to get the transform position value, use Vector2, to get it in a variable form.
+        Vector2 pos = transform.position;
+
+        //use Input,Getaxis to allow the x and y values to be modified when keys are pressed.
+        pos.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        pos.y += Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.position = pos;
     }
 }
